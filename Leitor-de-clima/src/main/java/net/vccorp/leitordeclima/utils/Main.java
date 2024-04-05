571c2d3c07bd0a459189670511e21e09488d34bc
pelopinho <plopes5544@gmail.com>
2024-04-05 20:58:13 -0300

    inserção da interface (codigo)
    codigo-base referente à interface inicial do software

--- Leitor-de-clima/src/main/java/net/vccorp/leitordeclima/utils/Main.java
+++ Leitor-de-clima/src/main/java/net/vccorp/leitordeclima/utils/Main.java
@@ -1,8 +1,43 @@
 package net.vccorp.leitordeclima.utils-de-clima.utils;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
 
-        System.out.println("Hello world!");
+
+    public class Main extends Application { //codigo-base da interface inicial
+
+        @Override
+        public void start(Stage primaryStage) {
+            primaryStage.setTitle("Upload de Arquivo CSV");
+
+            Button uploadButton = new Button("Selecionar Arquivo CSV");
+            uploadButton.setOnAction(event -> uploadCSVFile());
+
+            VBox vbox = new VBox(10);
+            vbox.getChildren().add(uploadButton);
+
+            Scene scene = new Scene(vbox, 300, 200);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        private void uploadCSVFile() {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivos CSV", "*.csv"));
+            File file = fileChooser.showOpenDialog(null);
+
+            if (file != null) {
+            }
+        }
+
+
+
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
-}
