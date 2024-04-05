1c7bd2c36df5349cdba5b47bef46720327600540
gabriel15asouza <gabalves.souza@gmail.com>
2024-04-05 20:00:05 -0300

    [feat] - project structure created

---
+++ Leitor-de-clima/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Leitor-de-clima"
+
---
+++ Leitor-de-clima/src/main/java/net/vccorp/leitordeclima/utils/Main.java
@@ -0,0 +1,8 @@
+package net.vccorp.leitordeclima.utils-de-clima.utils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
