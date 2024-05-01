905a97dd0a34023257528c7be4864a7cc1421669
Pablo <pablo.silva61@fatec.sp.gov.br>
2024-05-01 19:03:37 -0300

    [feat] Adiciona o arquivo Launcher.java, que contém a classe responsável por iniciar o app

---
+++ Leitor-clima/src/main/java/com/example/leitorclima/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.leitorclima;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
