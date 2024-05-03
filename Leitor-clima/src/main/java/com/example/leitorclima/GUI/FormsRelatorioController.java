3fc2a943eb8808dc376fbead7556b41a3ed69ab4
gabriel15asouza <gabalves.souza@gmail.com>
2024-05-03 01:01:54 -0300

    [feat] - calculo media de dados

--- Leitor-clima/src/main/java/com/example/leitorclima/GUI/FormsRelatorioController.java
+++ Leitor-clima/src/main/java/com/example/leitorclima/GUI/FormsRelatorioController.java
@@ -1,5 +1,6 @@
 package com.example.leitorclima.GUI;
 
+import com.example.leitorclima.Models.Registro;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,7 +14,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static com.example.leitorclima.Utils.DbUtils.*;
+//import static com.example.leitorclima.Utils.DbUtils.getArquivo;
 
 public class FormsRelatorioController implements Initializable {
 
@@ -29,6 +34,24 @@ public class FormsRelatorioController implements Initializable {
     private Button btnGeraRelatorio;
     @FXML
     private Button btnVoltaMenu;
+    private List<String> listaCidade;
+    private List<String> listaDado;
+    float chuva;
+    float dirVento;
+    float insolacao;
+    float nebulos;
+    float pressao;
+    float totalChuva;
+    float totalDirVento;
+    float totalInsolacao;
+    float totalNebulos;
+    float totalPresao;
+    float medPressao = 0;
+    float medChuva = 0;
+    float medDirVento = 0;
+    float medInsolacao = 0;
+    float medNebulos = 0;
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -42,11 +65,15 @@ public class FormsRelatorioController implements Initializable {
                 voltaMenu();
         });
 
-        cbCidade.getItems().add("a");
-        cbDado.getItems().add("b");
+        listaCidade = geraCidadeComboBox();
+        listaDado = geraDadoComboBox();
+
+        cbCidade.getItems().addAll(listaCidade);
+        cbDado.getItems().addAll(listaDado);
     }
 
     private void geraRelatorio() {
+        List<Registro> listaRegistro;
         String cidade = cbCidade.getValue();
         String dado = cbDado.getValue();
         String dataInicio = String.valueOf(dpDataInicio.getValue());
@@ -56,6 +83,80 @@ public class FormsRelatorioController implements Initializable {
         System.out.println(dado);
         System.out.println(dataInicio);
         System.out.println(dataFim);
+
+        listaRegistro = getArquivo(cidade,dataInicio,dataFim);
+
+        for(Registro registro: listaRegistro ){
+            if(registro.getIndice().equals("Chuva [Diaria] (mm)")){
+                chuva ++;
+                if (!registro.getValor().isEmpty()) {
+                    totalChuva += Float.parseFloat(registro.getValor());
+                } else {
+                    totalChuva += 0;
+                }
+            }else if (registro.getIndice().equals("Dir. Vento (m/s)")){
+                dirVento ++;
+                if (!registro.getValor().isEmpty()) {
+                    totalDirVento += Float.parseFloat(registro.getValor());
+                } else {
+                    totalDirVento += 0;
+                }
+            }else if (registro.getIndice().equals("Insolacao (h)")) {
+                insolacao++;
+                if (!registro.getValor().isEmpty()) {
+                    totalInsolacao += Float.parseFloat(registro.getValor());
+                } else {
+                    totalInsolacao += 0;
+                }
+            }else if (registro.getIndice().equals("Nebulosidade (Decimos)")) {
+                nebulos++;
+                if (!registro.getValor().isEmpty()) {
+                    totalNebulos += Float.parseFloat(registro.getValor());
+                } else {
+                    totalNebulos += 0;
+                }
+            }else if (registro.getIndice().equals("Pressao (hPa)")) {
+                pressao ++;
+                if (!registro.getValor().isEmpty()) {
+                    totalPresao += Float.parseFloat(registro.getValor());
+                } else {
+                    totalPresao += 0;
+                }
+            }
+        }
+        if (chuva != 0) {
+            medChuva = totalChuva/chuva;
+        }  else {
+            medChuva = 0;
+        }
+        if (dirVento != 0) {
+            medDirVento = totalDirVento/dirVento;
+        }  else {
+            medDirVento = 0;
+        }
+        if (insolacao != 0) {
+            medInsolacao = totalInsolacao/insolacao;
+        }  else {
+            medInsolacao = 0;
+        }
+        if (nebulos != 0) {
+            medNebulos = totalNebulos/nebulos;
+        }  else {
+            medNebulos = 0;
+        }
+        if (pressao != 0) {
+            medPressao = totalPresao/pressao;
+        }  else {
+            medPressao = 0;
+        }
+
+
+        System.out.println("--MEDIAS--");
+        System.out.println(medChuva);
+        System.out.println(medDirVento);
+        System.out.println(medInsolacao);
+        System.out.println(medNebulos);
+        System.out.println(medPressao);
     }
 
     private void voltaMenu() {
