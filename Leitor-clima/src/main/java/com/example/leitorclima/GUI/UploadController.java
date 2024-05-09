1efdac49097734a6a8c984456d843f15444cc922
Pablo <pablo.silva61@fatec.sp.gov.br>
2024-05-09 19:30:39 -0300

    [fix] - importando "Alert" e "AlertType" para funcionamento do popup

--- Leitor-clima/src/main/java/com/example/leitorclima/GUI/UploadController.java
+++ Leitor-clima/src/main/java/com/example/leitorclima/GUI/UploadController.java
@@ -11,7 +11,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.*;
 import java.net.URL;
