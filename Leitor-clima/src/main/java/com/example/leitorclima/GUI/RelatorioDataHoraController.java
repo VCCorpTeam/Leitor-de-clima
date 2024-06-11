544b71425ec1dcd92f046d28dfee5a07ccd2bdd5
pelopinho <143050296+pelopinho@users.noreply.github.com>
2024-06-10 21:20:09 -0300

    [FEAT] controller data/hora
    Co-Authored-By: hstcris <143611263+hstcris@users.noreply.github.com>

---
+++ Leitor-clima/src/main/java/com/example/leitorclima/GUI/RelatorioDataHoraController.java
@@ -0,0 +1,37 @@
+package com.example.leitorclima.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+    public class RelatorioDataHoraController {
+
+        @FXML
+        private TableView<?> tableView;
+
+        @FXML
+        private Button voltarButton;
+
+        @FXML
+        private TextField cidadeTextField;
+
+        @FXML
+        private TextField inicioTextField;
+
+        @FXML
+        private TextField fimTextField;
+
+        @FXML
+        private Button csvButton;
+
+        @FXML
+        private void initialize() {
+            // Aqui você pode adicionar lógica de inicialização, como configurar os listeners dos botões
+            // ou inicializar os dados da tabela
+        }
+
+        // Aqui você pode adicionar métodos para manipular a interação do usuário ou atualizar os dados na tabela
+    }
+
+
